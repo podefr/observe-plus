5f90c9e401412ad60bb0de44c98b5698b0fa2a39
Olivier Scherrer <pode.fr@gmail.com>
2015-01-28 23:48:05 -0500

    Cleanup + add comments

--- src/Observe.js
+++ src/Observe.js
@@ -9,28 +9,34 @@ var asap = require("asap");
 var loop = require("simple-loop"),
     nestedProperty = require("nested-property");
 
-function isValidValueToObserve(val) {
-    return val !== null && typeof val == "object";
-}
-
-function clone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
-
-function subtractPath(path1, path2) {
-    return path1.split(path2 + ".").join("");
-}
-
-function getValueFromPartialPath(fullPath, partialPath, object) {
-    return nestedProperty.get(object, subtractPath(fullPath, partialPath));
-}
-
+/**
+ * Observe can observe a top level object/array, or a nested object/array within itself
+ * @param observedObject the object/array being observed, can be top, or a nested one
+ * @param namespace if it's a nested array/object, the namespace is useful to remember the path from the top to itself
+ * @param callbacks if it's a nested array/object, we keep the callbacks from the top one
+ * @param rootObject if it's a nested array/object we keep a reference to the top rootObject
+ * @constructor
+ */
 module.exports = function Observe(observedObject, namespace, callbacks, rootObject) {
     var _callbacks = callbacks || {},
         _isPaused = false,
         _savedEvents = [],
         _prototype = null;
 
+    /**
+     * ------------------------------------------------------
+     * PUBLIC API
+     * ------------------------------------------------------
+     */
+
+    /**
+     * Add a listener for a given value of the event's object
+     * @param propertyName the property on the event object that we want to listen to
+     * @param propertyValue the value of the property for which we want to trigger an event
+     * @param callback the callback to execute
+     * @param scope within which scope
+     * @returns {dispose}
+     */
     this.addListener = function addListener(propertyName, propertyValue, callback, scope) {
         var item = [callback, scope];
         _callbacks[propertyName] = _callbacks[propertyName] || {};
@@ -49,6 +55,14 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         };
     };
 
+    /**
+     * Same as addListener, but disposes itself after being called
+     * @param propertyName the property on the event object that we want to listen to
+     * @param propertyValue the value of the property for which we want to trigger an event
+     * @param callback the callback to execute
+     * @param scope within which scope
+     * @returns {dispose}
+     */
     this.addListenerOnce = function addListenerOnce(propertyName, propertyValue, callback, scope) {
         var dispose = this.addListener(propertyName, propertyValue, function () {
             callback.apply(scope, arguments);
@@ -57,14 +71,25 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         return dispose;
     };
 
+    /**
+     * Pause triggering the events. They'll be published again when calling resume.
+     * In the meantime, you can safely apply any change to the observed object/array.
+     */
     this.pause = function pause() {
         _isPaused = true;
     };
 
+    /**
+     * Check to see if it's paused
+     * @returns {boolean}
+     */
     this.isPaused = function isPaused() {
         return _isPaused;
     };
 
+    /**
+     * Will trigger all the events that have been stashed while the observation was paused
+     */
     this.resume = function resume() {
         asap(function () {
             _isPaused = false;
@@ -73,25 +98,20 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         });
     };
 
+    /**
+     * Stop observing
+     */
     this.destroy = function destroy() {
         _prototype.unobserve(observedObject, treatEvents);
     };
 
-    if (observedObject === null || typeof observedObject != "object") {
-        throw new TypeError("observe must be called with an Array or an Object");
-    }
+    /**
+     * ------------------------------------------------------
+     * THE LOGIC FOR PUBLISHING EVENTS
+     * ------------------------------------------------------
+     */
 
-    _prototype = Array.isArray(observedObject) ? Array : Object;
-
-    if (typeof _prototype.observe != "function") {
-        throw new Error("Make sure that the harmony options are enabled in this runtime. " +
-            "Run node with the --harmony option or navigate to " +
-            "chrome://flags/#enable-javascript-harmony in Chrome. " +
-            "Check out https://github.com/podefr/observe-plus for more info");
-    }
-
-    _prototype.observe(observedObject, treatEvents);
-
+    // Either we publish the events, or we stash them for later
     function treatEvents(events) {
         if (_isPaused) {
             _savedEvents = _savedEvents.concat(events);
@@ -100,72 +120,105 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         }
     }
 
+    function executeCallback(newEvent, callbackArray) {
+        var callback = callbackArray[0],
+            thisObj = callbackArray[1];
+        try {
+            callback.call(thisObj, newEvent);
+        } catch (err) {
+        }
+    }
+
+    // When we have a bunch of fresh events, or stashed events, we loop through them to publish the relevant events
     function publishEvents(events) {
         events.forEach(function (ev) {
-            function executeCallback(newEvent, callbackArray) {
-                var callback = callbackArray[0],
-                    thisObj = callbackArray[1];
-                try {
-                    callback.call(thisObj, newEvent, ev);
-                } catch (err) {
-                }
-            }
-
             Object.keys(_callbacks).forEach(function (eventType) {
-                var callbacksForEventType = _callbacks[eventType],
-                    eventPropertyName = ev[eventType],
-                    newEvent, namespacedName;
-
-                if (eventType == "name") {
-
-                    newEvent = clone(ev);
-                    newEvent.object = rootObject || observedObject;
-
-                    if (ev.hasOwnProperty("name")) {
-                        namespacedName = namespace ? namespace + "." + ev.name : ev.name;
-                    } else {
-                        namespacedName = namespace ? namespace + "." + ev.index : ev.index;
-                    }
-
-                    loop(callbacksForEventType, function (callbacks, property) {
-                        if (nestedProperty.isIn(rootObject || observedObject, property, newEvent.object)) {
-                            if (newEvent.type === "update" &&
-                                getValueFromPartialPath(property, namespacedName, ev.oldValue) === nestedProperty.get(rootObject || observedObject, property)) {
-                                return;
-                            }
-                            if (newEvent.hasOwnProperty("name")) {
-                                newEvent.name = property;
-                                if (newEvent.type !== "add") {
-                                    newEvent.oldValue = getValueFromPartialPath(property, namespacedName, ev.oldValue);
-                                }
-                            } else {
-                                newEvent.index = property;
-
-                            }
-
-                            callbacks.forEach(executeCallback.bind(null, newEvent));
-                        }
-                    });
-
+                if (strategies[eventType]) {
+                    strategies[eventType](eventType, ev);
                 } else {
-                    newEvent = clone(ev);
-                    newEvent.object = rootObject || observedObject;
-
-                    if (newEvent.hasOwnProperty("name")) {
-                        newEvent.name = namespace ? namespace + "." + ev.name : ev.name;
-                    } else {
-                        newEvent.index = namespace ? namespace + "." + ev.index : ev.index;
-                    }
-
-                    if (callbacksForEventType && callbacksForEventType[eventPropertyName]) {
-                        callbacksForEventType[eventPropertyName].forEach(executeCallback.bind(null, newEvent));
-                    }
+                    strategies.defaut(eventType, ev);
                 }
-
             });
         });
     }
 
+    // When observing a "name" property, the logic is sligthly different than other properties
+    var strategies = {
+        name: function (eventType, ev) {
+            var namespacedName;
+
+            var newEvent = clone(ev);
+
+            newEvent.object = rootObject || observedObject;
+
+            if (ev.hasOwnProperty("name")) {
+                namespacedName = namespace ? namespace + "." + ev.name : ev.name;
+            } else {
+                namespacedName = namespace ? namespace + "." + ev.index : ev.index;
+            }
+
+            loop(_callbacks[eventType], function (callbacks, property) {
+                if (nestedProperty.isIn(rootObject || observedObject, property, newEvent.object)) {
+                    if (newEvent.type === "update" &&
+                        getValueFromPartialPath(property, namespacedName, ev.oldValue) === nestedProperty.get(rootObject || observedObject, property)) {
+                        return;
+                    }
+                    if (newEvent.hasOwnProperty("name")) {
+                        newEvent.name = property;
+                        if (newEvent.type !== "add") {
+                            newEvent.oldValue = getValueFromPartialPath(property, namespacedName, ev.oldValue);
+                        }
+                    } else {
+                        newEvent.index = property;
+
+                    }
+
+                    callbacks.forEach(executeCallback.bind(null, newEvent));
+                }
+            });
+        },
+        defaut: function (eventType, ev) {
+            var newEvent = clone(ev);
+            newEvent.object = rootObject || observedObject;
+
+            if (newEvent.hasOwnProperty("name")) {
+                newEvent.name = namespace ? namespace + "." + ev.name : ev.name;
+            } else {
+                newEvent.index = namespace ? namespace + "." + ev.index : ev.index;
+            }
+
+            if (_callbacks[eventType] && _callbacks[eventType][ev[eventType]]) {
+                _callbacks[eventType][ev[eventType]].forEach(executeCallback.bind(null, newEvent));
+            }
+        }
+    };
+
+    /**
+     * ------------------------------------------------------
+     * INITIALIZATION
+     * ------------------------------------------------------
+     */
+
+    // Make sure we only observe Arrays and Objects
+    if (observedObject === null || typeof observedObject != "object") {
+        throw new TypeError("observe must be called with an Array or an Object");
+    }
+
+    // Decide which Observe to use, Object.observe or Array.observe
+    _prototype = Array.isArray(observedObject) ? Array : Object;
+
+    // If no observe found, throw an error
+    if (typeof _prototype.observe != "function") {
+        throw new Error("Make sure that the harmony options are enabled in this runtime. " +
+            "Run node with the --harmony option or navigate to " +
+            "chrome://flags/#enable-javascript-harmony in Chrome. " +
+            "Check out https://github.com/podefr/observe-plus for more info");
+    }
+
+    // Actually observe
+    _prototype.observe(observedObject, treatEvents);
+
+    // And now, recursively walk the array/object to watch nested arrays/objects
     loop(observedObject, function (value, key) {
         var newNamespace;
 
@@ -176,6 +229,7 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         }
     });
 
+    // Also listen to add events, for when a new object/array is added!
     this.addListener("type", "add", function (event) {
         var value = event.object[event.name],
             key = event.name,
@@ -187,6 +241,7 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
         }
     });
 
+    // Same, but if a new item is added to an array
     this.addListener("type", "splice", function (event) {
         if (event.addedCount > 0) {
             event.object
@@ -200,5 +255,20 @@ module.exports = function Observe(observedObject, namespace, callbacks, rootObje
                 });
         }
     });
-
 };
+
+function isValidValueToObserve(val) {
+    return val !== null && typeof val == "object";
+}
+
+function clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+function subtractPath(path1, path2) {
+    return path1.split(path2 + ".").join("");
+}
+
+function getValueFromPartialPath(fullPath, partialPath, object) {
+    return nestedProperty.get(object, subtractPath(fullPath, partialPath));
+}
